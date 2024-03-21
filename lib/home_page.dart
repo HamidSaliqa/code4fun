02f2c41271b2d2aa558c83cfc56d3caf11fe7e0a
Hamid <HamidSaliqa>
2024-03-21 11:35:35 +0430

    تمیز کردن کد صفحه home_page . انتقال کد ها در Widget.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,7 +11,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    var libraryNames = ["About WASSA", "Our Services", "Who Can Benefit?","Student Project","Gallery","Contact"];
+
     return Scaffold(
       backgroundColor: Color.fromRGBO(217, 217, 217, 1),
       appBar: AppBar(
@@ -24,15 +24,7 @@ class _HomePageState extends State<HomePage> {
         children: [
           Expanded(child: slider()),
           Expanded(
-            child: GridView.count(
-              crossAxisCount: 2,
-              children: List.generate(libraryNames.length, (index) {
-                return CustomButton(
-                  nameOfButton: libraryNames[index],
-                  onPressed: () {},
-                );
-              }),
-            ),
+            child: customGridview(),
           ),
         ],
       )
@@ -40,29 +32,3 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class CustomButton extends StatelessWidget {
-  final String nameOfButton;
-  final VoidCallback onPressed;
-
-  CustomButton({required this.nameOfButton, required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          boxShadow: [
-        BoxShadow(color: Colors.black12, offset: Offset(2, 2), blurRadius: 8)
-      ]),
-      child: ElevatedButton(
-        style: ButtonStyle(
-          shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(21))),
-            backgroundColor: MaterialStatePropertyAll(Color.fromRGBO(3, 125, 214, 1))),
-        onPressed: onPressed,
-        child: Text(
-          nameOfButton,style: TextStyle(fontFamily:"CustomFont",fontSize: 30),
-        ),
-      ),
-    );
-  }
-}
--- lib/widget.dart
+++ lib/widget.dart
@@ -53,3 +53,47 @@ Widget slider(){
     isLoop: true,
   );
 }
+
+///Custom Button for home page
+
+class CustomButton extends StatelessWidget {
+  final String nameOfButton;
+  final VoidCallback onPressed;
+
+  CustomButton({required this.nameOfButton, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(color: Colors.black12, offset: Offset(2, 2), blurRadius: 8)
+          ]),
+      child: ElevatedButton(
+        style: ButtonStyle(
+            shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(21))),
+            backgroundColor: MaterialStatePropertyAll(Color.fromRGBO(3, 125, 214, 1))),
+        onPressed: onPressed,
+        child: Text(
+          nameOfButton,style: TextStyle(fontFamily:"CustomFont",fontSize: 30),
+        ),
+      ),
+    );
+  }
+}
+///names for home page Button
+var libraryNames = ["About WASSA", "Our Services", "Who Can Benefit?","Student Project","Gallery","Contact"];
+
+///Home page Grid view use custom Button
+ Widget customGridview(){
+  return GridView.count(
+    crossAxisCount: 2,
+    children: List.generate(libraryNames.length, (index) {
+      return CustomButton(
+        nameOfButton: libraryNames[index],
+        onPressed: () {},
+      );
+    }),
+  );
+}
