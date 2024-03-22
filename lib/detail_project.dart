f4da33752739790a9243a03c02d1cba23e75172d
Hamid <HamidSaliqa>
2024-03-22 23:56:03 +0430

    درست کردن صفحه detail Project

--- lib/detail_project.dart
+++ lib/detail_project.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class DetailsProject extends StatelessWidget {
   final String imagePath;
 
@@ -7,11 +8,91 @@ class DetailsProject extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image.asset(
-          imagePath,
-          fit: BoxFit.cover,
-        ),
+      backgroundColor: const Color.fromARGB(100, 94, 90, 90),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: ClipRRect(
+                      child: Image.asset(
+                        imagePath,
+                        fit: BoxFit.cover,
+                        width: 380,
+                        height: 217,
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Expanded(
+                      flex: 3,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Container(
+                          width: double.infinity,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            color: Color.fromRGBO(235, 235, 235, 1),
+                          ),
+                          child: Center(
+                              child: const Text(
+                            "Description",
+                            style: TextStyle(fontFamily: "CustomFont",fontSize: 30),
+                          )),
+                        ),
+                      )),
+                  SizedBox(
+                    height: 65,
+                    width: 350,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Color.fromARGB(255, 6, 149, 203),
+                        ),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(34),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "Project Link",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "CustomFont"),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
