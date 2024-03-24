8eaabf1f558eed987590802ae6b1b503dc1042c9
Hamid <HamidSaliqa>
2024-03-24 15:47:52 +0430

    اضافه کردن Expanded

--- lib/contact_us.dart
+++ lib/contact_us.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-
 class ContactUs extends StatelessWidget {
-  const ContactUs({super.key});
+  const ContactUs({super.key,});
 
   @override
   Widget build(BuildContext context) {
@@ -20,49 +19,58 @@ class ContactUs extends StatelessWidget {
             height: 50,
           ),
           const Align(
-              alignment: Alignment.centerLeft,
-              child: Padding(
-                padding: EdgeInsets.only(left: 40),
-                child: Text(
-                  "Have Any\nQuestions ?",
-                  style: TextStyle(fontFamily: "CustomFont", fontSize: 30),
-                ),
-              )),
-          const SizedBox(
-            height: 40,
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.only(left: 40),
+              child: Text(
+                "Have Any\nQuestions ?",
+                style: TextStyle(fontFamily: "CustomFont", fontSize: 30),
+              ),
+            ),
           ),
-          customRectengle(
-              width: 260,
-              child: const Text("+93728581122",
-                  style: TextStyle(fontFamily: "CustomFont", fontSize: 20))),
-          const SizedBox(
-            height: 10,
+          const Expanded(
+            child: SizedBox(
+              height: 40,
+            ),
           ),
-          customRectengle(
-              width: 300,
+          Expanded(
+            child: customRectengle(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: const Text(
+                "+93728581122",
+                style: TextStyle(fontFamily: "CustomFont", fontSize: 20),
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Expanded(
+            child: customRectengle(
+              width: MediaQuery.of(context).size.width * 0.9,
               child: const Text(
                 "info@wassa.org.af",
                 style: TextStyle(fontFamily: "CustomFont", fontSize: 20),
-              )),
-          const SizedBox(
-            height: 10,
+              ),
+            ),
           ),
+          SizedBox(height: 10,),
           customRectengle(
-              width: 340,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  InkWell(child: Image.asset("assets/twitter.png")),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  InkWell(child: Image.asset("assets/telegram.png")),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  InkWell(child: Image.asset("assets/instagram.png")),
-                ],
-              )),
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InkWell(child: Image.asset("assets/twitter.png")),
+                const SizedBox(
+                  width: 10,
+                ),
+                InkWell(child: Image.asset("assets/telegram.png")),
+                const SizedBox(
+                  width: 10,
+                ),
+                InkWell(child: Image.asset("assets/instagram.png")),
+              ],
+            ),
+          ),
+          Expanded(child: SizedBox(height: 20,)),
         ],
       ),
     );
